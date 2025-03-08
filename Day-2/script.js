c9180ad0b02e733867c1c0ff9e0672a4d98c1114
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-03-08 08:11:53 +0530

    Add files via upload

---
+++ Day-2/script.js
@@ -0,0 +1,27 @@
+console.log("Hello I'm Conditionals JS");
+
+let age = 5;
+let grace = 2;
+
+if((age+grace) > 18){
+    console.log("you can drive");
+}
+else{
+    console.log("you can't drive");
+}
+ 
+// Operators in Js
+// Arithmetic operators --> + - * / % ++ --
+
+console.log(age+grace);
+console.log(age-grace);
+console.log(age*grace);
+console.log(age/grace);
+console.log(age%grace);
+
+// ternary operator --> condition ? true : false 
+let a = 4;
+let b = 8;
+let c = a>b ? "a is greater" : "b is greater";
+console.log(c);
+

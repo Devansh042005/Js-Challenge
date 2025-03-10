e7dc95e54fe9d53db45a76b5eda9498436a07da2
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-03-10 23:42:53 +0530

    Add files via upload

---
+++ Day-6/script.js
@@ -0,0 +1,7 @@
+console.log("Harry");
+let boxes = document.getElementsByClassName("box");
+console.log(boxes);
+
+boxes[2].style.backgroundColor = "red"; 
+
+document.querySelector(".box").style.backgroundColor = "blue";
